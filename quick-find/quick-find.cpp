ce79dd5b879ede9fab6067d52ad9a0368ef3cb09
sterluan <luanster@gmail.com>
2015-08-28 18:42:27 -0400

    add quick-find

--- quick-find/quick-find.cpp
+++ quick-find/quick-find.cpp
@@ -1,1 +1,31 @@
-#
+class QuickFind {
+Public:
+    QuickFind(int size);
+    int find(int p);
+    bool connected(int p, int q);
+    int count();
+    void unite(int p, int q);
+    
+private:
+    int cnt; // number of different groups
+    vector<int> id;
+};
+
+QuickFind::QuickFind(int size) : cnt(size), id(size, 0) {
+    for (int i = 0; i < size; ++i) id[i] = i;
+}
+
+int QuickFind::find(int p) { return id[p]; }
+
+bool QuickFind::connected(int p, int q) { return find[p] == find[q]; }
+
+int QuickFind::count() { return cnt; }
+
+void QuickFind::unite(int p, int q) {
+    int pId = find(p), qId = find(q);
+    if (pId == qId) return;
+    --cnt;
+    for (int i = 0; i < id.size(); ++i) {
+        if (id[i] == pId) id[i] = qId;
+    }
+}
